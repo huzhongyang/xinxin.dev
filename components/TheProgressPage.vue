0f023473b4433e605333891b760f0cec41bb510d
zyH <hzy18023@gmail.com>
2024-06-27 17:02:46 +0800

    feat: change alignment style

--- components/TheProgressPage.vue
+++ components/TheProgressPage.vue
@@ -33,57 +33,59 @@ function useTyped(from: HTMLElement, to: HTMLElement, speed: number = 10) {
 
 <template>
   <div class="w-screen flex flex-col select-none items-center justify-center gap-4 h-lvh">
-    <div class="text-5xl font-700" flex="~ row items-center justify-center gap-1">
-      <nuxt-img
-        data-cursor="block"
-        data-cursor-style="--cursor-radius: 50%;--cursor-scale: 1.1;--cursor-blur-duration: 1.4s"
-        src="/images/avatar.png" alt="avatar"
-        class="avatar h-3rem p1 drag-none"
-        border="~ red/60 rounded-full"
-      />
-      <div>
-        <span data-cursor="text" class="p-y-2 text-center">Hi, I'm&nbsp;</span>
-        <span data-cursor="block" class="name-hover inline-block p-y-2">zhongyang</span>
-      </div>
-    </div>
-
-    <div flex="~ row items-center">
-      <div ref="desTypedRef" inline-block data-cursor="text" class="text-24px font-500" />
-    </div>
-
-    <div ref="desTypedEleRef" hidden>
-      <div>
-        <span data-cursor="text">
-          A Curious ^200 fullstack
-        </span>
-
-        <div ml1 inline-block px1 italic>
-          <code data-cursor="block" leading-none data-cursor-style="radius: 6px">
-            &#60;developer&nbsp;/&#62;
-          </code>
+    <div flex="~ col items-start justify-center gap-4">
+      <div class="text-5xl font-700" flex="~ row items-center justify-center gap-1">
+        <nuxt-img
+          data-cursor="block"
+          data-cursor-style="--cursor-radius: 50%;--cursor-scale: 1.1;--cursor-blur-duration: 1.4s"
+          src="/images/avatar.png" alt="avatar"
+          class="avatar h-3rem p1 drag-none"
+          border="~ red/60 rounded-full"
+        />
+        <div>
+          <span data-cursor="text" class="p-y-2 text-center">Hi, I'm&nbsp;</span>
+          <span data-cursor="block" class="name-hover inline-block p-y-2">zhongyang</span>
         </div>
       </div>
-    </div>
 
-    <div flex="~ row items-center" class="text-gray/40">
-      <div ref="noteTypedRef" inline-block data-cursor="text" />
-    </div>
+      <div flex="~ row items-center">
+        <div ref="desTypedRef" inline-block data-cursor="text" class="text-24px font-500" />
+      </div>
 
-    <div ref="noteTypedEleRef" hidden>
-      <div>
-        <div flex="~" items-center>
-          <div data-cursor="block" mr2 p1 class="i-fluent-emoji:face-with-peeking-eye" />
+      <div ref="desTypedEleRef" hidden>
+        <div>
           <span data-cursor="text">
-            I'm building this site
+            A Curious ^200 fullstack
           </span>
+
+          <div ml1 inline-block px1 italic>
+            <code data-cursor="block" leading-none data-cursor-style="radius: 6px">
+              &#60;developer&nbsp;/&#62;
+            </code>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div>
-      <nuxt-link drag-none to="https://github.com/" target="_blank">
-        <div class="i-carbon-logo-github h-8 w-8 transition-all duration-200 hover:scale-110 hover:opacity-80" />
-      </nuxt-link>
+      <div flex="~ row items-center" class="text-gray/40">
+        <div ref="noteTypedRef" inline-block data-cursor="text" />
+      </div>
+
+      <div ref="noteTypedEleRef" hidden>
+        <div>
+          <div flex="~" items-center>
+            <div data-cursor="block" mr2 p1 class="i-fluent-emoji:face-with-peeking-eye" />
+            <span data-cursor="text">
+              I'm building this site
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-4">
+        <nuxt-link drag-none to="https://github.com/huzhongyang" target="_blank">
+          <div class="i-carbon-logo-github h-8 w-8 transition-all duration-200 hover:scale-110 hover:opacity-80" />
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
