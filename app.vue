bafe98ce1142df30cdde69cd7b36884f63f84c78
zyH <hzy18023@gmail.com>
2024-07-25 21:33:34 +0800

    style: revert

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@ setupHeader()
 </script>
 
 <template>
-  <div class="w-screen h-lvh" bg="~ gray" bg-gradient="to-45 from-gray-500 via-gray-600 to-gray-700">
+  <div class="w-screen h-lvh">
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
@@ -17,12 +17,5 @@ setupHeader()
 html,
 body {
   --uno: font-akaya;
-}
-
-.root-bg {
-  background-color: gray;
-  background-image: linear-gradient(45deg, #000 5%, #fff 5%),
-    linear-gradient(#000 5%, #fff 5%);
-  background-size: 20px 20px;
 }
 </style>
