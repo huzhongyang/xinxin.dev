0e9d1b20922906f9b6bee86bc17523c6b8b12f5d
zyH <hzy18023@gmail.com>
2024-10-29 00:18:04 +0800

    refactor: remove ClientOnly tag from Menu component

--- components/leftSide/TheMenu.vue
+++ components/leftSide/TheMenu.vue
@@ -9,23 +9,19 @@ function menuClicked(menu: Menu) {
 </script>
 
 <template>
-  <div>
-    <client-only>
-      <nuxt-link
-        v-for="menu in menuRouter" :key="menu.name"
-        :to="menu.path"
-        grid="~ rows-1 cols-[1rem_1fr] items-center gap-3"
-        my-2 p-2 duration-400
-        border="rounded-2xl"
-        hover:shadow="~ rounded-2xl"
-        :class="[curPath === menu.name ? 'cur-path' : 'hover:menu-hover-bg']"
-        @click="menuClicked(menu)"
-      >
-        <div h-4 w-4 :class="[menu.icon, menu.name === curPath ? 'bg-white' : '']" />
-        <span>{{ menu.name }}</span>
-      </nuxt-link>
-    </client-only>
-  </div>
+  <nuxt-link
+    v-for="menu in menuRouter" :key="menu.name"
+    :to="menu.path"
+    grid="~ rows-1 cols-[1rem_1fr] items-center gap-3"
+    my-2 p-2 duration-400
+    border="rounded-2xl"
+    hover:shadow="~ rounded-2xl"
+    :class="[curPath === menu.name ? 'cur-path' : 'hover:menu-hover-bg']"
+    @click="menuClicked(menu)"
+  >
+    <div h-4 w-4 :class="[menu.icon, menu.name === curPath ? 'bg-white' : '']" />
+    <span>{{ menu.name }}</span>
+  </nuxt-link>
 </template>
 
 <style scoped>
