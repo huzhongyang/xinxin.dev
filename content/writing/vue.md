c690e6720494f35d3c0e23d5cf5d37059c8def25
zy <hzy18023@gmail.com>
2025-02-04 23:15:42 +0800

    fix: change modules list order and load image asset

--- content/writing/vue.md
+++ content/writing/vue.md
@@ -1,1 +1,40 @@
 ### vue
+
+`hefwfwweeweweweweweweweweeweweweweeq`
+**he**
+
+``Use `code` in your Markdown file.``
+
+```
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+```
+
+```json
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+```
+
+去露营了！ :tent: 很快回来。
+
+真好笑！ :joy:
+
+- [x] Write the press release
+- [ ] Update the website
+- [ ] Contact the media
+
+| Syntax      | Description |
+| ----------- | ----------- |
+| Header      | Title       |
+| Paragraph   | Text        |
+
+| Syntax      | Description | Test Text     |
+| :---        |    :----:   |          ---: |
+| Header      | Title       | Here's this   |
+| Paragraph   | Text        | And more      |
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,24 @@ import process from 'node:process'
 
 export default defineNuxtConfig({
   extends: '@nuxt-themes/typography',
-  modules: ['@unocss/nuxt', '@nuxtjs/google-fonts', '@nuxt/image', '@nuxt/content', 'pinceau/nuxt'],
+  modules: ['pinceau/nuxt', '@unocss/nuxt', '@nuxtjs/google-fonts', '@nuxt/image', '@nuxt/content'],
+  content: {
+    build: {
+      markdown: {
+        highlight: {
+          // Theme used in all color schemes.
+          theme: {
+            // Default theme (same as single string)
+            default: 'github-light',
+            // Theme used if `html.dark`
+            dark: 'github-dark',
+            // Theme used if `html.sepia`
+            sepia: 'monokai',
+          },
+        },
+      },
+    },
+  },
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
